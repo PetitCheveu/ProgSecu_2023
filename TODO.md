3aee9f72d251ff36d4a6b9c6a0e33609ee6c0aed
PetitCheveu <elenabeylat@gmail.com>
2023-10-06 16:04:04 +0200

    fix(cli) : multiples requetes client

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+# Conseils du prof 1 : 
 Ne pas utiliser les signaux car risque d'arrêt de procédure systèmes... -> comportements ératiques
 
 Erreur de buffer dans les tubes présentés dans les aides du TP. Les deux restent en attente car le message est plus court que le buffer. Pour contrer cela, on ouvre et on ferme les tubes dans la boucle. Sinon, il faut générer le flush.
@@ -15,7 +16,7 @@ mémoire partagée : multiprossessing
 processus voir
 socket voir le moment venu -> existe en python pour du multiclient, pas nécessaire dans notre sujet, mais à voir
 
-TODO : 
+# TODO : 
 Gestion des erreurs : Assurez-vous de gérer les erreurs correctement dans votre code, en utilisant des blocs try/except pour capturer les exceptions qui pourraient survenir lors de l'ouverture de fichiers, de la création de sockets, etc. Cela rendra votre code plus robuste.
 
 Documentation : Ajoutez des commentaires et de la documentation aux parties importantes de votre code pour expliquer ce que fait chaque fonction et chaque section. Cela rendra votre code plus compréhensible pour les autres et pour vous-même à l'avenir.
@@ -39,3 +40,33 @@ Test et débogage : Testez votre code étape par étape pour vous assurer que ch
 Réactivité du serveur principal : La question 3.a concerne la réactivité du serveur principal face aux redémarrages initiés par le watchdog. Cela dépendra de la nature de votre application et de vos besoins spécifiques, mais il est important de considérer comment votre système doit réagir en cas de redémarrage.
 
 Utilisation de signaux : La question 3.c suggère d'utiliser des signaux pour la surveillance. Les signaux peuvent être utiles pour détecter si un processus est figé, mais cela nécessite une gestion de signaux appropriée dans votre code.
+
+# Conseils du prof 2 : 
+## Protocole d'échanges (de messages)
+### Solution idéale
+1) Requête du client au serveur principal (fait)
+2) Délégation de traitement du serveur principal au serveur secondaire (fait)
+3) Acceptation de la requête (début traitement) + transmission d'infos du serveur secondaire au serveur principal à destination du client (N° port ...) (?)
+4) Transmission de l'adresse, IP X N° port du client (?)
+5) Client transmet ses données au serveur secondaire (fait)
+6) Serveur secondaire envoie résultat de requête au client (fait)
+7) Serveurs secondaires informe serveur principal de la fin de traitement de requête.
+
+### Solution autre
+1) Requête du client au SP
+2) SP-> SS
+3) SS -> SP
+4) SS -> Client (qui il est)
+5) Client -> SS (requête)
+6) SS -> Client (réponse)
+
+### Fonctionnement en états : 
+0.. -> Etat du client
+1.. -> Etat du serveur principal
+2.. -> Etat du serveur secondaire
+
+0 -> Envoi d'une requête au serveur principal
+1 -> Attente d'une réponse du serveur principal (sollution 1) ou secondaire (solution 2) -> c'est sa position d'attente
+10 -> Attente d'une requête client ou watchdog -> c'est sa position d'attente -> Attente de deux évènements différents, les deux par sockets ? utilisation de timeout
+20 -> Attente d'authorisation de lecture dans le segment mémoire partagé ou du watchdog -> Pas en attente sur une socket, mais sur un tube -> Position d'attente
+21 -> Séparer l'attente du watchdog pour le faire après
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import time
 import socket
 import json
 import random
+import select
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # def run_client():
 #     print("Client started.")
@@ -36,8 +40,12 @@ import random
 #     secondary_socket.close()
 #     client_socket.close()
 
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
 def run_client():
-    print("Client started.")
+    logging.info("Client started.")
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 2222))
@@ -64,10 +72,10 @@ def run_client():
             # Échanger des informations
             secondary_socket.sendall(b"Hello, secondary server!")
             data = secondary_socket.recv(1024)
-            print(f"Client received from secondary server: {data.decode()}")
+            logging.info(f"Client received from secondary server: {data.decode()}")
 
         except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            logging.info(f"An error occurred: {str(e)}")
 
         finally:
             # Fermer la connexion au serveur secondaire
@@ -83,7 +91,7 @@ def run_client():
 
 # Fonctions pour le dispatcher (serveur principal)
 def run_dispatcher():
-    print("Dispatcher started.")
+    logging.info("Dispatcher started.")
     
     # Utiliser un fichier comme mémoire partagée
     with open('shared_memory.txt', 'w') as f:
@@ -95,43 +103,49 @@ def run_dispatcher():
     for _ in range(3):
         os.write(fifo_out, b'ping')
         with open('wdtube1', 'r') as fifo_in:
-            print(f"Dispatcher received: {fifo_in.read().strip()}")
+            logging.info(f"Dispatcher received: {fifo_in.read().strip()}")
     
     # Créer un socket pour accepter les connexions du client
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Réutiliser le port
     server_socket.bind(('localhost', 2222))
     server_socket.listen(1)
     
     conn, addr = server_socket.accept()
     server_socket.close()
-    print(f"Dispatcher connected to client at {addr}")
+    logging.info(f"Dispatcher connected to client at {addr}")
     
     while True:
         try:
             data = conn.recv(1024)
             if not data:
                 break
-            print(f"Dispatcher received: {data.decode()}")
+            logging.info(f"Dispatcher received: {data.decode()}")
             conn.sendall(b"2223")
         except ConnectionResetError:
-            print("Client disconnected unexpectedly.")
+            logging.info("Client disconnected unexpectedly.")
             break
     
+    server_socket.close()
     conn.close()
 
 # Fonctions pour le worker (serveur secondaire)
 def run_worker():
-    print("Worker started.")
+    logging.info("Worker started.")
     
     # Lire depuis la mémoire partagée
+    touch('shared_memory.txt')
     with open('shared_memory.txt', 'r') as f:
-        print(f"Worker received from Shared Memory: {f.read().strip()}")
+        logging.info(f"Worker received from Shared Memory: {f.read().strip()}")
     
     fifo_in = os.open('dwtube1', os.O_RDONLY)
+
+    # Ouvrir le tube nommé pour la lecture
+    stop_fifo_in = os.open('stop_tube', os.O_RDONLY | os.O_NONBLOCK)
     
     for _ in range(3):
         msg = os.read(fifo_in, 4).decode()
-        print(f"Worker received from Dispatcher: {msg}")
+        logging.info(f"Worker received from Dispatcher: {msg}")
         with open('wdtube1', 'w') as fifo_out:
             fifo_out.write("pong")
     
@@ -139,37 +153,55 @@ def run_worker():
 
     # Créer un socket pour accepter les connexions du client
     worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    worker_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Réutiliser le port
+    worker_socket.setblocking(0)  # Set the socket to non-blocking
     worker_socket.bind(('localhost', 2223))
     worker_socket.listen(1)
     
-    conn, addr = worker_socket.accept()
-    print(f"Worker connected to client at {addr}")
-    
     while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        print(f"Worker received: {data.decode()}")
-        
-        # Envoyer des données de réponse au client
-        conn.sendall(b"Hello, client!")
+        # Vérifier si 'stop' a été écrit dans le tube
+        try:
+            stop_msg = os.read(stop_fifo_in, 4).decode()
+            if stop_msg == 'stop':
+                logging.info("Worker received stop signal. Exiting.")
+                break
+        except BlockingIOError:
+            pass
+
+        ready, _, _ = select.select([worker_socket], [], [], 1)
+        if ready:
+            conn, addr = worker_socket.accept()
+            logging.info(f"Worker connected to client at {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                logging.info(f"Worker received: {data.decode()}")
+                
+                # Envoyer des données de réponse au client
+                conn.sendall(b"Hello, client!")
     
+    worker_socket.close()
     conn.close()
 
 # Fonctions pour le watchdog
 def run_watchdog():
-    print("Watchdog started.")
+    logging.info("Watchdog started.")
     
     # Créer des tubes nommés
     if not os.path.exists('dwtube1'):
         os.mkfifo('dwtube1')
     if not os.path.exists('wdtube1'):
         os.mkfifo('wdtube1')
+
+    # Créer un autre tube nommé pour stopper le worker
+    if not os.path.exists('stop_tube'):
+        os.mkfifo('stop_tube')
     
     # Lancer le dispatcher
     pid = os.fork()
     if pid < 0:
-        print("fork() impossible")
+        logging.info("fork() impossible")
         os.abort()
     elif pid == 0:
         run_dispatcher()
@@ -178,27 +210,30 @@ def run_watchdog():
         # Lancer le worker
         worker_pid = os.fork()
         if worker_pid < 0:
-            print("fork() impossible")
+            logging.info("fork() impossible")
             os.abort()
         elif worker_pid == 0:
             run_worker()
             os._exit(0)
+
+        else:
+            # Watchdog observe les processus (ici, il attend simplement)
+            time.sleep(30)
+            with open('stop_tube', 'w') as stop_fifo:
+                stop_fifo.write('stop')
+                logging.info("Watchdog: Checking process health.")
         
-        # Watchdog observe les processus (ici, il attend simplement)
-        time.sleep(10)
-        print("Watchdog: Checking process health.")
-        
-        # Nettoyage : supprimer les tubes nommés et la mémoire partagée
-        if os.path.exists('dwtube1'):
-            os.remove('dwtube1')
-        if os.path.exists('wdtube1'):
-            os.remove('wdtube1')
-        if os.path.exists('shared_memory.txt'):
-            os.remove('shared_memory.txt')
+                # Nettoyage : supprimer les tubes nommés et la mémoire partagée
+                if os.path.exists('dwtube1'):
+                    os.remove('dwtube1')
+                if os.path.exists('wdtube1'):
+                    os.remove('wdtube1')
+                if os.path.exists('shared_memory.txt'):
+                    os.remove('shared_memory.txt')
 
 if __name__ == '__main__':
     print("1: Run Watchdog (and servers)")
-    print("2: Run Client")
+    ("2: Run Client")
     choice = input("Choose an option: ")
     
     if choice == '1':
@@ -206,5 +241,4 @@ if __name__ == '__main__':
     elif choice == '2':
         run_client()
     else:
-        print("Invalid option.")
-
+        logging.debug("Invalid option.")
