976a571bdf6598e2980fa5906f9428e98fed0fcc
PetitCheveu <elenabeylat@gmail.com>
2023-10-27 13:16:47 +0200

    feat: simple communication btw D & WD

--- dispatcher.py
+++ dispatcher.py
@@ -9,8 +9,31 @@ import struct
 
 def run_dispatcher():
     logging.info("Dispatcher started.")
+    try : 
+        watchdog_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        watchdog_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        watchdog_socket.bind(('localhost', 2224))
+        watchdog_socket.listen(1)
+        logging.info("Watchdog socket created and listening.")
+        connexion_watchdog, addresse = watchdog_socket.accept()
+        logging.info(f"Dispatcher connected to watchdog at {addresse}")
+    except Exception as e :
+        logging.error(f"An error occurred during during the creation of the socket between the dispatcher and the watchdog: {e}")
 
     while True:
+        try : 
+            data = connexion_watchdog.recv(1024)
+            if not data:
+                break
+            watchdog_question = data.decode()
+            logging.info(f'Dispatcher received: {watchdog_question}')
+            if watchdog_question == "Still running ?" : 
+                connexion_watchdog.sendall(b"Yes")
+            elif watchdog_question == "Ca fonctionne toujours ?" : 
+                connexion_watchdog.sendall(b"Oui")
+            
+        except Exception as e : 
+            logging.error(f"An error occurred: {e}")
         try:
             # Checking that the connexion is possible and that the port is correctly closed
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,13 +42,10 @@ def run_dispatcher():
             server_socket.listen(1)
         except OSError as e:
             logging.warning(f"Port 2222 is already in use. Closing the previous socket and retrying.")
-            if server_socket:
-                server_socket.close()
-            os.kill(pid, signal.SIGTERM)
             logging.info("Dispatcher process terminated.")
             time.sleep(1)  # Wait for a short time before retrying
             break
-        
+            
         conn, addr = server_socket.accept()
         server_socket.close()
         logging.info(f"Etape 1 : Dispatcher connected to client at {addr}")
--- watchdog.py
+++ watchdog.py
@@ -13,32 +13,33 @@ import dispatcher
 def close_processes(pid, worker_pid):
     logging.info("Watchdog: Closing process healthly.")
     
-    # Terminer le processus du dispatcher
-    os.kill(pid, signal.SIGTERM)
-    logging.info("Dispatcher process terminated.")
+    # Terminer les processus
+    close_process(pid, "Dispatcher")
+    close_process(worker_pid, "Worker")
     
-    # Terminer le processus du worker
-    os.kill(worker_pid, signal.SIGTERM)
-    logging.info("Worker process terminated.")
-
-    if os.path.exists('dwtube1'):
-        os.remove('dwtube1')
-    if os.path.exists('wdtube1'):
-        os.remove('wdtube1')
+    delete_tube('dwtube1')
+    delete_tube('wdtube1')
+    
     if os.path.exists('shared_memory.txt'):
         os.remove('shared_memory.txt')
     exit(0)
+    
+def close_process(pid, type_process):
+    os.kill(pid, signal.SIGTERM)
+    logging.info(type_process+" process terminated.")
 
+def delete_tube(tube):
+    if os.path.exists(tube):
+        os.remove(tube)
 
 def run_watchdog():
     logging.info("Watchdog started.")
 
-    
     if not os.path.exists('dwtube1'):
         os.mkfifo('dwtube1')
     if not os.path.exists('wdtube1'):
         os.mkfifo('wdtube1')
-    
+
     pid = os.fork()
     if pid < 0:
         logging.info("fork() impossible")
@@ -56,17 +57,35 @@ def run_watchdog():
             os._exit(0)
         else:
             try:
+                time.sleep(0.1)
+                watchdog_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                dispatcher_address = ('localhost', 2224)
+                watchdog_socket.connect(dispatcher_address)
                 logging.info("Press Ctrl+C to close")
-                signal.pause()  # Attendre jusqu'à ce que Ctrl+C soit pressé
+
+                while True:
+                    logging.info("test")
+                    choice = random.choice([b"Still running ?", b"Ca fonctionne toujours ?"])
+                    watchdog_socket.sendall(choice)
+                    timeout = time.time() + 100000
+                    logging.debug(f"timeout : {timeout}")
+                    try : 
+                        response = watchdog_socket.recv(1024)
+                        response_dispatcher = response.decode()
+                        logging.info(f"Watchdog received from Dispatcher : {response_dispatcher}")
+                        if response.decode() != "Yes" and response.decode() != "Oui":
+                            close_processes(pid, worker_pid)
+                            break
+                    except time.time() > timeout :
+                        logging.error("Timeout, restarting the serveur")
+                        close_processes(pid, worker_pid)
+                        # restart_serveur()
+                        break
+                    time.sleep(5)
+
             except KeyboardInterrupt:
                 close_processes(pid, worker_pid)
             except Exception as e:
                 logging.error(f"An error occurred during termination: {e}")
-            # try:
-            #     time.sleep(5)
-            #     logging.info("Ctrl+C to close")
-
-            #     close_processes(pid, worker_pid)
-            # except Exception as e:
-            #     logging.error(f"An error occurred during termination: {e}")
-                
+            finally:
+                watchdog_socket.close()
