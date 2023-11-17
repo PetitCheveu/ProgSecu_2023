dd0d35d526006832511288b0b1aa5e66d7b3c977
PetitCheveu <elenabeylat@gmail.com>
2023-11-17 16:42:55 +0100

    doc : worker and dispatcher

--- dispatcher.py
+++ dispatcher.py
@@ -1,3 +1,13 @@
+"""
+Dispatcher Module
+
+This module defines a Dispatcher that communicates with a Watchdog, listens for client requests, and coordinates with a Worker.
+
+The Dispatcher manages communication with the Watchdog and Worker to handle client requests, check the Worker's availability, and signal the Worker to start or finish its tasks.
+
+Author: Elena BEYLAT & Robin JOSEPH
+"""
+
 import time
 import socket
 import select
@@ -7,21 +17,33 @@ import mmap
 import os
 
 def run_dispatcher():
+    """
+    Run the Dispatcher process.
+
+    This function initializes the Dispatcher, communicates with the Watchdog, listens for client requests, and coordinates with the Worker.
+
+    """
+    # Log the start of the Dispatcher process
     logging.info("Dispatcher started.")
 
+    # Shared memory configuration
     shared_memory_file = '/tmp/shared_memory'
     SHARED_MEMORY_SIZE = 1024
 
+    # Ensure the '/tmp' directory exists
     if not os.path.exists('/tmp'):
         os.makedirs('/tmp')
 
+    # Create and truncate the shared memory file
     with open(shared_memory_file, 'wb') as f:
         f.truncate(SHARED_MEMORY_SIZE)
 
+    # Initialize shared memory
     with open(shared_memory_file, 'r+b') as f:
         shared_memory = mmap.mmap(f.fileno(), SHARED_MEMORY_SIZE)
 
     try : 
+        # Set up the socket connection with the Watchdog
         watchdog_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         watchdog_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         watchdog_socket.bind(('localhost', 2224))
@@ -34,11 +56,14 @@ def run_dispatcher():
 
     while True:
         try : 
+            # Receive data from the Watchdog
             data = connexion_watchdog.recv(1024)
             if not data:
                 break
             watchdog_question = data.decode()
             logging.info(f'Dispatcher received: {watchdog_question}')
+            
+            # Respond to the Watchdog based on the received question
             if watchdog_question == "Still running ?" : 
                 connexion_watchdog.sendall(b"Yes")
             elif watchdog_question == "Tu fonctionnes toujours ?" : 
@@ -48,29 +73,27 @@ def run_dispatcher():
         except Exception as e : 
             logging.error(f"An error occurred: {e}")
         try:
-            # Checking that the connexion is possible and that the port is correctly closed
+            # Set up the server socket to listen for client requests
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.bind(('localhost', 2222))
             server_socket.listen(1)
         except OSError as e:
+            # Handle the case where port 2222 is already in use
             logging.warning(f"Port 2222 is already in use. Closing the previous socket and retrying.\n\n{e}")
             logging.info("Dispatcher process terminated.")
             break
             
         conn, addr = None, None
-        start_time = time.time()  # Définir le temps de départ
-        while conn is None and (time.time() - start_time) < 1:  # Attendre jusqu'à 4 secondes
+        start_time = time.time()  # Set the start time
+        while conn is None and (time.time() - start_time) < 1:  # Wait for up to 1 second
             ready_sockets, _, _ = select.select([server_socket], [], [], 4.0)
             if ready_sockets:
                 conn, addr = server_socket.accept()
-                break  # Sortir de la boucle si une connexion est établie
+                break  # Exit the loop if a connection is established
             else:
                 logging.info("Waiting for client request...")
 
-        # if conn is None:
-        #     logging.info("No client, still waiting")
-        #     continue
         if conn is None:
             logging.info("No client, still waiting")
 
@@ -95,29 +118,38 @@ def run_dispatcher():
         if conn is not None:
             logging.debug(f"Etape 1 : Dispatcher connected to client at {addr}")
             try:
+                # Receive data from the client
                 data = conn.recv(1024)
                 if not data:
                     break
                 request_from_client = data.decode()
                 logging.debug(f"Etape 1 : Dispatcher received: {request_from_client}")
 
+                # Set shared memory to indicate that the worker is needed
                 shared_memory.seek(0)
                 shared_memory.write(b'\x00' * SHARED_MEMORY_SIZE)
                 shared_memory.seek(0)
                 shared_memory.write(b"Are you free for a connexion ?\x00")
                 
+                # Notify the worker via named pipe
                 with open('dwtube1', 'w') as fifo_out: 
                     fifo_out.write('ping')
+                    
+                # Receive response from the worker via named pipe
                 with open('wdtube1', 'r') as fifo_in:
                     logging.debug(f"Dispatcher received from Worker: {fifo_in.read().strip()}")
                 
+                # Read the worker's answer from shared memory
                 shared_memory.seek(0)
                 answer = shared_memory.read(SHARED_MEMORY_SIZE).decode().strip('\x00')
 
                 
                 if answer == "Yes" : 
                     logging.debug(f"Etape 3 : Worker is free to work")
+                    # Send the worker's listening port to the client
                     conn.sendall(b"2223")
+                    
+                    # Close the client connection and server socket
                     try:
                         conn.close()
                         server_socket.close()
@@ -125,17 +157,21 @@ def run_dispatcher():
                         logging.debug(e)
                     
 
+                    # Set shared memory to indicate that the worker's task is done
                     shared_memory.seek(0)
                     shared_memory.write(b'\x00' * SHARED_MEMORY_SIZE)                   
                     shared_memory.seek(0)
                     shared_memory.write(b"Are you done ?\x00")
                 
+                    # Notify the worker via named pipe
                     with open('dwtube1', 'w') as fifo_out: 
                         fifo_out.write('ping')
                         
+                    # Receive response from the worker via named pipe
                     with open('wdtube1', 'r') as fifo_in:
                         logging.info(f"Dispatcher received from Worker: {fifo_in.read().strip()}")
                     
+                    # Read the worker's completion status from shared memory
                     shared_memory.seek(0)
                     is_worker_done = shared_memory.read(SHARED_MEMORY_SIZE).decode().strip('\x00')
                     
--- worker.py
+++ worker.py
@@ -1,3 +1,13 @@
+"""
+Worker Module
+
+This module defines a Worker that interacts with a Dispatcher and communicates with a Client using sockets and shared memory.
+
+The Worker receives requests from the Dispatcher, checks its availability, and responds to the Client with the current time or date.
+
+Author: Elena BEYLAT & Robin JOSEPH
+"""
+
 import socket
 import select
 import logging
@@ -6,11 +16,20 @@ import mmap
 import datetime
 
 def run_worker():
+    """
+    Run the Worker process.
+
+    This function initializes the Worker, communicates with the Dispatcher, and responds to Client requests with the current time or date.
+
+    """
+    # Log the start of the Worker process
     logging.info("Worker started.")
 
+    # Shared memory configuration
     shared_memory_file = '/tmp/shared_memory'
     SHARED_MEMORY_SIZE = 1024
 
+    # Initialize shared memory
     with open(shared_memory_file, 'rb+') as f:
         f.seek(SHARED_MEMORY_SIZE - 1)
         f.write(b'\x00')
@@ -19,20 +38,25 @@ def run_worker():
     with open(shared_memory_file, 'r+b') as f:
         shared_memory = mmap.mmap(f.fileno(), SHARED_MEMORY_SIZE)
     
+    # Initialize socket-related variables
     conn = None
     connected = False
     
     while True:
+        # Read from the Dispatcher's named pipe
         with open('dwtube1', 'r') as fifo_out:
             logging.info(f"Worker received from Dispatcher: {fifo_out.read().strip()}")
 
+        # Read the question from shared memory
         shared_memory.seek(0)
         question = shared_memory.read(SHARED_MEMORY_SIZE).decode().strip('\x00')
 
+        # Handle different types of questions
         if question == "Are you free for a connexion ?" :
             logging.debug(f"Etape 2 : Worker is asked for disponibility")
         
             if connected == False :
+                # Set shared memory to 'Yes' and notify the Dispatcher
                 shared_memory.seek(0)
                 shared_memory.write(b'\x00' * SHARED_MEMORY_SIZE)
                 
@@ -43,21 +67,25 @@ def run_worker():
                 writen = shared_memory.read(SHARED_MEMORY_SIZE).decode().strip('\x00')
                 logging.debug(f"Writen memory {writen}")
 
+                # Notify the Dispatcher via named pipe
                 with open('wdtube1', 'w') as fifo_in:
                     fifo_in.write('pong')     
                 
+                # Set up a socket to listen for connections
                 worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 worker_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 worker_socket.setblocking(0)
                 worker_socket.bind(('localhost', 2223))
                 worker_socket.listen(1)
                 
+                # Check for incoming connection within a timeout
                 ready, _, _ = select.select([worker_socket], [], [], 1)
                 if ready:
-    
                     conn, addr = worker_socket.accept()
                     logging.debug(f"Etape 5 : Worker connected to client at {addr}")
                     connected = True
+                    
+                    # Handle data received from the Client
                     while True:
                         data = conn.recv(1024)
                         request = data.decode()
@@ -65,6 +93,7 @@ def run_worker():
                             break
                         logging.debug(f"Etape 5 : Worker received: {request}")
                         
+                        # Generate a response based on the request
                         now = datetime.datetime.now()
                         message = "no response"
                         if request == "Hello, what time is it ?" : 
@@ -76,11 +105,14 @@ def run_worker():
                             message = "Hello, client! we are the " + response
                             message = message.encode('utf-8')
                         conn.sendall(message)
+                
+                # Close the worker socket and the connection
                 worker_socket.close()
                 if conn is not None : 
                     conn.close()
                 connected = False
             else :
+                # Set shared memory to 'No' and notify the Dispatcher
                 shared_memory.seek(0)
                 shared_memory.write(b'\x00' * SHARED_MEMORY_SIZE)
                 shared_memory.seek(0)
@@ -92,14 +124,18 @@ def run_worker():
         elif question == "Are you done ?":
             if connected == False : 
                 logging.info(f"Worker read: {question}")
+                
+                # Set shared memory to 'Yes' and notify the Dispatcher
                 shared_memory.seek(0)
                 shared_memory.write(b'\x00' * SHARED_MEMORY_SIZE)
                 shared_memory.seek(0)
                 shared_memory.write(b"Yes\x00")
 
+                # Notify the Dispatcher via named pipe
                 with open('wdtube1', 'w') as fifo_in:
                     fifo_in.write('pong')
         elif question == "":
+            # Notify the Dispatcher via named pipe
             with open('wdtube1', 'w') as fifo_in:
                 fifo_in.write('pong')
 
